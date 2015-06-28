051d56bb9e34f86316bb66c560b0376f51c4fa3d
chjphp <chjphp@sina.cn>
2015-06-28 14:28:03 +0800

    delete file

--- kuaidi/demo.php
+++
@@ -1,7 +0,0 @@
-<?php
-require ('./Express.php');
-header('Content-Type:text/html; charset=utf-8');
-$express = new Express();
-$result  = $express -> getorder('shunfeng','906343289973');
-var_dump($result);
-?>
